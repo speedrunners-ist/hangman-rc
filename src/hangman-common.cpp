7e179b1f1090a1a756ec8d80b909c6c8eacbd509
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-11-25 10:57:49 +0000

    Add project skeleton - cpp and header files for both client and server

---
+++ src/hangman-common.cpp
@@ -0,0 +1,5 @@
+#include <hangman-common.h>
+
+// Below, TCP-related functions
+
+// Below, UDP-related functions
---
+++ src/hangman-common.h
@@ -0,0 +1,18 @@
+#ifndef HANGMAN_COMMON_H
+#define HANGMAN_COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define PORT "58050" // TODO: change this to the correct port number (i.e 58000 + group number)
+
+#endif
+
+// Below, function prototypes
