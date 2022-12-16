24759c15e9527e54b1013383acd55a57ef673bb3
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-12-16 19:48:17 +0000

    Add test script (#73)

---
+++ test.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+MY_IP=$(curl -s http://ipecho.net/plain)
+TEJO="nc tejo.tecnico.ulisboa.pt 59000"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+function run_server() {
+  server/GS server/word_eng.txt -v > tests/tmp/server.log
+}
+
+function run_command() {
+    echo "Testing $1"
+    FILE_NAME=$(basename $1 .txt)
+    TEST=$(echo $FILE_NAME | cut -d'_' -f2)
+    echo "Test: $TEST"
+    COMMAND=$(echo "$MY_IP 58001 $TEST" | $TEJO > tests/tmp/report-$TEST.html)
+    if [ ! -s tests/tmp/report-$TEST.html ] || grep -q "error" tests/tmp/report-$TEST.html; then
+        echo -e "${RED}Test failed${NC}"
+    else
+        echo -e "${GREEN}Test passed${NC}"
+    fi
+}
+
+function handle_test() {
+    run_server &
+    run_command $1
+    killall GS
+    sleep 5 # to avoid being timed out ;-;
+}
+
+# Script core
+
+echo "You can find the results of each test in the tests/tmp folder"
+
+# create the tests/tmp folder if it doesn't exist
+mkdir -p tests/tmp
+# clear it
+rm -rf tests/tmp/*
+
+for test in tests/scripts/*.txt
+do
+    handle_test $test
+done
