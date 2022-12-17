c39a1059202e16bec43233c3ec5ab445f394e23e
Pesteves2002 <tomasesteves2002@gmail.com>
2022-12-17 13:45:04 +0000

    Fix common

--- common/common.cpp
+++ common/common.cpp
@@ -61,7 +61,8 @@ int GameState::correctGuess(std::string positions, int n) {
       std::cerr << INVALID_POSITIONS_ERROR << std::endl;
       setWord(initialWord);
       return -1;
-    } else if (word[posNum - 1] != '_') {
+    }
+    if (word[posNum - 1] != '_') {
       std::cerr << ALREADY_FILLED_ERROR << std::endl;
       setWord(initialWord);
       return -1;
@@ -238,7 +239,8 @@ int receiveUDPMessage(char *response, size_t maxBytes, struct addrinfo *res, int
   if (bytesReceived == -1) {
     std::cerr << RECVFROM_ERROR << std::endl;
     return -1;
-  } else if (response[bytesReceived - 1] != '\n') {
+  }
+  if (response[bytesReceived - 1] != '\n') {
     std::cerr << UDP_RESPONSE_ERROR << std::endl;
     return -1;
   }
@@ -257,10 +259,18 @@ int messageUDPHandler(int fd, struct addrinfo *res, protocolMessage &message, re
 /*** TCP message parsing/sending implementation ***/
 
 int parseTCPMessage(std::string request, protocolMessage &serverMessage) {
-  std::string responseBegin = request;
-  const std::string command = responseBegin.substr(0, 3);
-  responseBegin.erase(0, 4);
-  const std::string plid = responseBegin.substr(0, responseBegin.find_first_of(" \n"));
+  std::string responseBegin;
+  std::string plid;
+  std::string command;
+  try {
+    responseBegin = request;
+    command = responseBegin.substr(0, 3);
+    responseBegin.erase(0, 4);
+    plid = responseBegin.substr(0, responseBegin.find_first_of(" \n"));
+  } catch (const std::exception &e) {
+    std::cerr << TCP_RESPONSE_ERROR << std::endl;
+    return -1;
+  }
   serverMessage.first = command;
   serverMessage.second = plid;
   serverMessage.body = request;
@@ -320,7 +330,7 @@ int receiveTCPMessage(std::string &message, int args, int fd) {
   int readArgs = 0;
   char c;
   do {
-    // FIXME: will there be a problem if the response is "ERR\n"?
+    // TODO: will there be a problem if the response is "ERR\n"?
     bytesReceived = read(fd, &c, 1);
     if (bytesReceived == -1) {
       std::cerr << TCP_RECV_MESSAGE_ERROR << std::endl;
@@ -406,7 +416,8 @@ int parseFileArgs(fileInfo &info, int fd) {
 int initialAvailableMistakes(int wordLength) {
   if (wordLength <= 6) {
     return 7;
-  } else if (7 >= wordLength && wordLength <= 10) {
+  }
+  if (7 >= wordLength && wordLength <= 10) {
     return 8;
   }
   return 9;
