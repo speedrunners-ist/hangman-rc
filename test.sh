83196988f8d2c4dd047de2b7e23d9213726eedbc
Tomás Esteves <69385653+Pesteves2002@users.noreply.github.com>
2022-12-19 16:52:48 +0000

    Add Makefile and script improvements (#101)
    Co-authored-by: randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>

--- test.sh
+++ test.sh
@@ -2,24 +2,27 @@
 
 MY_IP=$(curl -s http://ipecho.net/plain)
 TEJO="nc tejo.tecnico.ulisboa.pt 59000"
+TOTAL_TESTS=$(ls tests/scripts/*.txt | wc -l)
+CORRECT_TESTS=0
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 function run_server() {
-  server/GS server/word_eng.txt -v > tests/tmp/server.log
+  server/GS server/word_eng.txt -v -p 58045 > tests/tmp/server.log
 }
 
 function run_command() {
     echo "Testing $1"
     FILE_NAME=$(basename $1 .txt)
     TEST=$(echo $FILE_NAME | cut -d'_' -f2)
-    COMMAND=$(echo "$MY_IP 58001 $TEST" | $TEJO > tests/tmp/report-$TEST.html)
+    COMMAND=$(echo "$MY_IP 58045 $TEST" | $TEJO > tests/tmp/report-$TEST.html)
     if [ ! -s tests/tmp/report-$TEST.html ] || grep -q "color=\"red\"" tests/tmp/report-$TEST.html; then
         echo -e "${RED}Test failed${NC}"
     else
         echo -e "${GREEN}Test passed${NC}"
+        CORRECT_TESTS=$((CORRECT_TESTS+1))
     fi
 }
 
@@ -27,7 +30,7 @@ function handle_test() {
     run_server &
     run_command $1
     killall GS
-    sleep 3 # to avoid being timed out ;-;
+    sleep 2 # to avoid being timed out ;-;
 }
 
 # Script core
@@ -43,3 +46,13 @@ for test in tests/scripts/*.txt
 do
     handle_test $test
 done
+
+echo "Passed ${GREEN}$CORRECT_TESTS${NC} out of ${BLUE}$TOTAL_TESTS${NC} tests"
+
+if [ $CORRECT_TESTS -eq $TOTAL_TESTS ]; then
+    echo -e "${GREEN}All tests passed!${NC}"
+    exit 0
+else
+    echo -e "${RED}Some tests failed!${NC}"
+    exit -1
+fi
