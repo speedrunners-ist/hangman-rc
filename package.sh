d01cc4dcb3d780ff0190b504840445a2bd792422
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-12-22 10:41:33 +0000

    Add teacher's note to README + clang-format to package script

--- package.sh
+++ package.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-rm -f docs/proj_$1.zip
-mkdir -p docs/proj_$1
-cp -r client server lib README.md Makefile autodep docs/proj_$1
+rm -f docs/proj$1.zip
+mkdir -p docs/proj$1
+cp -r client server lib README.md Makefile autodep .clang-format docs/proj$1
 cd docs
-zip -r proj_$1.zip proj_$1
+rm -rf proj$1/server/assets/hints
+zip -r proj$1.zip proj$1
